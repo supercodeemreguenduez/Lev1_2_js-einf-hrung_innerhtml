664df4ea80c1639cef77d5c9246c23c37f9bf6f3
supercodemereguenduez <supercode.emreguenduez@gmail.com>
2022-04-20 15:29:22 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("info").innerHTML = "<h1>Hello World</h1>";
+
+document.getElementById("info").innerHTML += "<h2>How are you?</h2>";
+
+document.getElementById("container").innerHTML = "<p>start of the element</p>";
+
+document.write("end of the element");
